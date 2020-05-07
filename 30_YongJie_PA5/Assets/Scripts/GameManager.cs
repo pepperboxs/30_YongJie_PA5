bc6a320225d4401e9c6caff9f4e5079ff76a8e22
pepperboxs <58922915+pepperboxs@users.noreply.github.com>
2020-05-08 00:14:32 +0900

    Finished Mazed Game
    Finished Mazed Game

---
+++ 30_YongJie_PA5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private float Score;
+    public Text ScoreText;
+
+
+    void Update()
+    {
+        if (Score >= 4)
+        {
+            YouWin();
+        }
+    }
+
+    public void AddScore()
+    {
+        Score++;
+        ScoreText.text = "Coins Collected = " + Score;
+    }
+
+    void YouWin()
+    {
+        ScoreText.text = "You Win!";
+        SceneManager.LoadScene("GameWin");
+    }
+
+    public void YouLose()
+    {
+        SceneManager.LoadScene("GameLose");
+    }
+
+}
---
+++ 30_YongJie_PA5/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody PlayerRigidBody;
+    public GameManager GameManager;
+
+    void Start()
+    {
+        PlayerRigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 Movement = new Vector3(moveHorizontal, 0, moveVertical);
+        PlayerRigidBody.AddForce(Movement * Speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Coins")
+        {
+            Destroy(collision.gameObject);
+            GameManager.AddScore();
+        }
+
+        if (collision.gameObject.tag == "Walls")
+        {           
+            GameManager.YouLose();
+        }
+    }
+
+}
